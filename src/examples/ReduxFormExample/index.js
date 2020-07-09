808ae66228038e224bac757e4d8ad06e5e5c7d71
Fedor Yakubovich <f.yakubovich@gmail.com>
2020-07-09 14:38:08 +0300

    UPDATE examples using react-hooks

--- src/examples/ReduxFormExample/index.js
+++ src/examples/ReduxFormExample/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
 import createStore from './store';
 import Form from './formContainer';
 
 const store = createStore();
 
-const ReduxFormExample = () => {
+function ReduxFormExample() {
   return (
     <Provider store={store}>
       <Form />
     </Provider>
   );
-};
+}
 
 export default ReduxFormExample;
--- src/lib/star.js
+++ src/lib/star.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import Gradient from './gradient';
 
-const StarSVG = ({
+function StarSVG({
   viewBox,
   size,
   onMouseMove,
@@ -19,7 +19,7 @@ const StarSVG = ({
   fullId,
   halfId,
   noneId,
-}) => {
+}) {
   if (index <= value) {
     fill = `url(#${fullId})`;
   } else if (isHalf && Math.ceil(value) === index) {
@@ -53,13 +53,13 @@ const StarSVG = ({
       <Gradient {...props} />
       <polygon
         fill={fill}
-        points="153,230.775 247.35,299.625 211.65,187.425 306,121.125 191.25,121.125 153,6.375 114.75,121.125 0,121.125 
+        points="153,230.775 247.35,299.625 211.65,187.425 306,121.125 191.25,121.125 153,6.375 114.75,121.125 0,121.125
       94.35,187.425 58.65,299.625"
         pointerEvents="none"
       />
     </svg>
   );
-};
+}
 
 StarSVG.propTypes = {
   viewBox: PropTypes.string,
